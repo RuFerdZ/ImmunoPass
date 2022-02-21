046b2f8eb0f3734773b3412980ba8bd11ed31c53
root <root@Rusiru-PC.localdomain>
2022-02-22 00:03:51 +0530

    version upgrade

--- app/src/App.js
+++ app/src/App.js
@@ -5,25 +5,126 @@ import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import idl from './idl.json';
 
+import { PhantomWalletAdapter, SolletWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const wallets = [ new PhantomWalletAdapter(), new SolletWalletAdapter() ];
+
+const { SystemProgram, Keypair } = web3;
+const baseAccount = Keypair.generate();
+const opts = {
+  preflightCommitment: "processed"
+}
+const programID = new PublicKey(idl.metadata.address);
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [value, setValue] = useState('');
+  const [dataList, setDataList] = useState([]);
+  const [input, setInput] = useState('');
+  const wallet = useWallet()
+
+  async function getProvider() {
+    /* create the provider and return it to the caller */
+    /* network set to local network for now */
+    const network = "http://127.0.0.1:8899";
+    const connection = new Connection(network, opts.preflightCommitment);
+
+    const provider = new Provider(
+        connection, wallet, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  // async function initialize() {
+  //   const provider = await getProvider();
+  //   /* create the program interface combining the idl, program ID, and provider */
+  //   const program = new Program(idl, programID, provider);
+  //   try {
+  //     /* interact with the program via rpc */
+  //     await program.rpc.initialize("Hello World", {
+  //       accounts: {
+  //         baseAccount: baseAccount.publicKey,
+  //         user: provider.wallet.publicKey,
+  //         systemProgram: SystemProgram.programId,
+  //       },
+  //       signers: [baseAccount]
+  //     });
+
+  //     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  //     console.log('account: ', account);
+  //     setValue(account.data.toString());
+  //     setDataList(account.dataList);
+  //   } catch (err) {
+  //     console.log("Transaction error: ", err);
+  //   }
+  // }
+
+  // async function update() {
+  //   if (!input) return
+  //   const provider = await getProvider();
+  //   const program = new Program(idl, programID, provider);
+  //   await program.rpc.update(input, {
+  //     accounts: {
+  //       baseAccount: baseAccount.publicKey
+  //     }
+  //   });
+
+  //   const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  //   console.log('account: ', account);
+  //   setValue(account.data.toString());
+  //   setDataList(account.dataList);
+  //   setInput('');
+  // }
+
+  if (!wallet.connected) {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
+          <WalletMultiButton />
+        </div>
+    )
+  } else {
+    return (
+        <div className="App">
+          <div>
+            {
+                // !value && (<button onClick={initialize}>Initialize</button>)
+            }
+
+            {
+              value ? (
+                  <div>
+                    <h2>Current value: {value}</h2>
+                    <input
+                        placeholder="Add new data"
+                        onChange={e => setInput(e.target.value)}
+                        value={input}
+                    />
+                    {/* <button onClick={update}>Add data</button> */}
+                  </div>
+              ) : (
+                  <h3>Please Inialize.</h3>
+              )
+            }
+            {
+              dataList.map((d, i) => <h4 key={i}>{d}</h4>)
+            }
+          </div>
+        </div>
+    );
+  }
 }
 
-export default App;
+const AppWithProvider = () => (
+    <ConnectionProvider endpoint="http://127.0.0.1:8899">
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+)
+
+export default AppWithProvider;
