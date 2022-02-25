b9422a90692ac085caf1285dc085ad20fc72882e
RuFerdZ <ge.rusiru@gmail.com>
2022-02-25 13:45:26 +0530

    fixed dependency conflicts and hot reload

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import { Connection, PublicKey } from '@solana/web3.js';
-import { Program, Provider, web3 } from '@project-serum/anchor';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import {
+  Program, Provider, web3
+} from '@project-serum/anchor';
 import idl from './idl.json';
-
-import { PhantomWalletAdapter, SolletWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { getPhantomWallet, getSolflareWallet, getSolletWallet } from '@solana/wallet-adapter-wallets';
 import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 require('@solana/wallet-adapter-react-ui/styles.css');
 
-const wallets = [ new PhantomWalletAdapter(), new SolletWalletAdapter() ];
-
+const wallets = [
+  getPhantomWallet(),
+  getSolflareWallet(),
+  getSolletWallet()
+]
+/* const network = clusterApiUrl('devnet'); */
+const network = "http://127.0.0.1:8899";
 const { SystemProgram, Keypair } = web3;
 const baseAccount = Keypair.generate();
 const opts = {
   preflightCommitment: "processed"
 }
 const programID = new PublicKey(idl.metadata.address);
-
 
 function App() {
   const [value, setValue] = useState('');
@@ -29,102 +33,102 @@ function App() {
   async function getProvider() {
     /* create the provider and return it to the caller */
     /* network set to local network for now */
-    const network = "http://127.0.0.1:8899";
     const connection = new Connection(network, opts.preflightCommitment);
 
     const provider = new Provider(
-        connection, wallet, opts.preflightCommitment,
+      connection, wallet, opts.preflightCommitment,
     );
     return provider;
   }
 
-  // async function initialize() {
-  //   const provider = await getProvider();
-  //   /* create the program interface combining the idl, program ID, and provider */
-  //   const program = new Program(idl, programID, provider);
-  //   try {
-  //     /* interact with the program via rpc */
-  //     await program.rpc.initialize("Hello World", {
-  //       accounts: {
-  //         baseAccount: baseAccount.publicKey,
-  //         user: provider.wallet.publicKey,
-  //         systemProgram: SystemProgram.programId,
-  //       },
-  //       signers: [baseAccount]
-  //     });
+  async function initialize() {
+    const provider = await getProvider();
+    /* create the program interface combining the idl, program ID, and provider */
+    const program = new Program(idl, programID, provider);
+    try {
+      /* interact with the program via rpc */
+      await program.rpc.initialize("Hello World", {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
 
-  //     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  //     console.log('account: ', account);
-  //     setValue(account.data.toString());
-  //     setDataList(account.dataList);
-  //   } catch (err) {
-  //     console.log("Transaction error: ", err);
-  //   }
-  // }
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      console.log('account: ', account);
+      setValue(account.data.toString());
+      setDataList(account.dataList);
+    } catch (err) {
+      console.log("Transaction error: ", err);
+    }
+  }
 
-  // async function update() {
-  //   if (!input) return
-  //   const provider = await getProvider();
-  //   const program = new Program(idl, programID, provider);
-  //   await program.rpc.update(input, {
-  //     accounts: {
-  //       baseAccount: baseAccount.publicKey
-  //     }
-  //   });
+  async function update() {
+    if (!input) return
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+    await program.rpc.update(input, {
+      accounts: {
+        baseAccount: baseAccount.publicKey
+      }
+    });
 
-  //   const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  //   console.log('account: ', account);
-  //   setValue(account.data.toString());
-  //   setDataList(account.dataList);
-  //   setInput('');
-  // }
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('account: ', account);
+    setValue(account.data.toString());
+    setDataList(account.dataList);
+    setInput('');
+  }
 
-  if (!wallet.connected) {
+  if(!wallet.connected) {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
-          <WalletMultiButton />
-        </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
+        <WalletMultiButton />
+      </div>
     )
   } else {
     return (
-        <div className="App">
-          <div>
-            {
-                // !value && (<button onClick={initialize}>Initialize</button>)
-            }
+      <div className='App'>
+        <div>
+          {
+            !value && (<button onClick={initialize}>Intialize</button>)
+          }
 
-            {
-              value ? (
-                  <div>
-                    <h2>Current value: {value}</h2>
-                    <input
-                        placeholder="Add new data"
-                        onChange={e => setInput(e.target.value)}
-                        value={input}
-                    />
-                    {/* <button onClick={update}>Add data</button> */}
-                  </div>
-              ) : (
-                  <h3>Please Inialize.</h3>
-              )
-            }
-            {
-              dataList.map((d, i) => <h4 key={i}>{d}</h4>)
-            }
-          </div>
+          {
+            value ? (
+              <div>
+                jfjfjfj hfhhfhf
+                <h2>Current value: {value}</h2>
+                <input 
+                  placeholder='Add new data'
+                  onChange={e => setInput(e.target.value)}
+                  value={input}
+                />
+                <button onClick={update}>Add Data</button>
+              </div>
+            ) : (
+              <h3>Please Initialize</h3>
+            )
+          }
+          {
+            dataList.map((d, i) => <h4 key={i}>{d}</h4>)
+          }
         </div>
-    );
+      </div>  
+    )
   }
 }
 
 const AppWithProvider = () => (
-    <ConnectionProvider endpoint="http://127.0.0.1:8899">
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <App />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+  <ConnectionProvider endpoint={network}>
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
 )
 
 export default AppWithProvider;
