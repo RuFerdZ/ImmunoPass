8a1c9af966637b6208b1fd6fb855f22729989d17
RuFerdZ <ge.rusiru@gmail.com>
2022-06-18 01:21:12 +0530

    updated ph pages

--- app/src/pages/passport_holder/PassportHolderDashboard.jsx
+++ app/src/pages/passport_holder/PassportHolderDashboard.jsx
@@ -5,24 +5,7 @@ import { getPassportHolderByWalletAddress, getVaccinationRecordsOfPassportHolder
 import UserNotFound from '../UserNotFound'
 import { styled } from '@mui/material/styles';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-
-import {
-  Chart,
-  PieSeries,
-  Title
-} from '@devexpress/dx-react-chart-material-ui';
-  
-
-
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -31,34 +14,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { useNavigate } from "react-router-dom";
-
-
-import QRCode from "react-qr-code";
-
-// import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
 import { useWallet } from '@solana/wallet-adapter-react';
 import ViewRecordModal from '../ViewRecordModal';
-// import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  border: '2px solid #191c24',
-  boxShadow: 24,
-  p: 4,
-};
 
 // table configuration
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -82,24 +39,9 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(date, vaccine, batchNumber, dosage, status) {
+  return { date, vaccine, batchNumber, dosage, status };
 }
-
-const rows = [
-  createData('13-09-2022', 'Sinopharm', 'SN12390881451', '5 kg', 'VALID'),
-  createData('02-11-2021', 'Pfizer', 'PF9089031841', '3 shots', 'VALID'),
-  createData('11-09-2022', 'Astrazenaca', 'AZ09124901nxX', '20 g', 'PENDING'),
-  createData('18-09-2022', 'Moderna', 'MD92141515MM', '3 km', 'INVALID'),
-  createData('10-09-2022', 'BioNTech', 'BT1240980989015', '7 Watts', 'VALID'),
-];
-
-const data = [
-  { argument:'Pending', value:10 },
-  { argument:'Rejected', value:40 },
-  { argument:'Approved', value:70 },
-];
-
 
 export default function PassportHolderDashboard() {
 
@@ -107,45 +49,61 @@ export default function PassportHolderDashboard() {
   const wallet = useWallet();
   const [ passportHolder, setPassportHolder ] = useState({});
   const [ vaccines, setVaccines ] = useState([]);
-
-  // QR modal settings
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [rows, setRows] = useState([])
 
   // vaccine model settings
   const [openVaccine, setOpenVaccine] = React.useState(false);
   const handleOpenVaccine = () => setOpenVaccine(true);
   const handleCloseVaccinee = () => setOpenVaccine(false);
 
+  // var accountKey = ""
+
   useEffect(() => {
-    // console.log('wallet address -', wallet.publicKey);
     loadPH()
-    loadVaccines()
   }, []);
 
+  useEffect(() => {
+    if (passportHolder?.publicKey){
+      loadVaccines(passportHolder?.publicKey);
+    }
+  },[passportHolder])
+
+  useEffect(() => {
+    setRows([])
+    if (vaccines) {
+      createRecords(vaccines)
+    }
+  }, [vaccines])
 
   const loadPH = async () => {
     const ph = await getPassportHolderByWalletAddress(wallet);
-    console.log(ph?.account)
-    setPassportHolder(ph?.account);
+    console.log("passportHolder - ", ph?.account)
+    setPassportHolder(ph);
   }
-  // console.log("passport holder - " , passportHolder)
 
-  const loadVaccines = async () => {
-    const vaccines = await getVaccinationRecordsOfPassportHolder(wallet, passportHolder.publicKey);
-    setVaccines(vaccines);
+  const loadVaccines = async (accountKey) => {
+    const vac = await getVaccinationRecordsOfPassportHolder(wallet, accountKey);
+    console.log("vacccine - ", vac)
+    setVaccines(vac);
   }
-  // console.log("vaccines - " , vaccines)
+
+  const createRecords = (vacs) => {
+    vacs.forEach(vaccine => {
+      let row = createData(vaccine?.account?.createdDate, vaccine?.account?.vaccine, vaccine?.account?.batchNumber, vaccine?.account?.dosage, vaccine?.account?.status)
+      setRows(rows.concat(row))
+      console.log("rows - ", row)
+    })
+    console.log(rows)
+  }
 
   const getDateFormatted = (timestamp) => {
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    let options = { year: 'numeric', month: 'long', day: 'numeric' };
     const date = new Date(timestamp * 1000);
     return date.toLocaleDateString('en-US', options);
   }
 
   const getStatus = (status) => {
-    if (status === 'VALID') {
+    if (status === 'COMPLETED' || status === "VALID") {
       return <span style={{color: 'green', border: '1px solid'}}>{status}</span>
     } else if (status === 'INVALID') {
       return <span style={{color: 'red', border: '1px solid'}}>{status}</span>
@@ -172,13 +130,13 @@ export default function PassportHolderDashboard() {
                   <div className="secondary-text text-uppercase text-center mt-3 mb-4">
                     Your Information
                   </div>
-                  <div className="mt-2 px-5"><span className="text-bold mr-2">Identification Number: </span>{passportHolder.nic}</div>
-                  <div className="mt-2 px-5"><span className="text-bold mr-2">Gender: </span>{passportHolder.gender}</div>
-                  <div className="mt-2 px-5"><span className="text-bold mr-2">Phone: </span>{passportHolder.phone}</div>
-                  <div className="mt-2 px-5"><span className="text-bold mr-2">Address: </span>{passportHolder.address}</div>
-                  <div className="mt-2 px-5"><span className="text-bold mr-2">Date of Birth: </span>{getDateFormatted(passportHolder.dateOfBirth)}</div>
-                  <div className="mt-2 px-5"><span className="text-bold mr-2">Place of Birth: </span>{passportHolder.placeOfBirth}</div>
-                  <div className="mt-2 px-5"><span className="text-bold mr-2">Joined Date: </span>{getDateFormatted(passportHolder.joinedDate)}</div>
+                  <div className="mt-2 px-5"><span className="text-bold mr-2">Identification Number: </span>{passportHolder?.account?.nic}</div>
+                  <div className="mt-2 px-5"><span className="text-bold mr-2">Gender: </span>{passportHolder?.account?.gender}</div>
+                  <div className="mt-2 px-5"><span className="text-bold mr-2">Phone: </span>{passportHolder?.account?.phone}</div>
+                  <div className="mt-2 px-5"><span className="text-bold mr-2">Address: </span>{passportHolder?.account?.address}</div>
+                  <div className="mt-2 px-5"><span className="text-bold mr-2">Date of Birth: </span>{getDateFormatted(passportHolder?.account?.dateOfBirth)}</div>
+                  <div className="mt-2 px-5"><span className="text-bold mr-2">Place of Birth: </span>{passportHolder?.account?.placeOfBirth}</div>
+                  <div className="mt-2 px-5"><span className="text-bold mr-2">Joined Date: </span>{getDateFormatted(passportHolder?.account?.joinedDate)}</div>
                   <div className="wallet-container mt-3">
                       <WalletMultiButton />
                   </div>
@@ -210,13 +168,11 @@ export default function PassportHolderDashboard() {
                   <TableBody>
                     {rows.map((row) => (
                       <StyledTableRow key={row.name}>
-                        <StyledTableCell component="th" scope="row">
-                          {row.name}
-                        </StyledTableCell>
-                        <StyledTableCell align="left">{row.calories}</StyledTableCell>
-                        <StyledTableCell align="left">{row.fat}</StyledTableCell>
-                        <StyledTableCell align="left">{row.carbs}</StyledTableCell>
-                        <StyledTableCell align="left">{getStatus(row.protein)}</StyledTableCell>
+                        <StyledTableCell component="th" scope="row">{getDateFormatted(row.date)}</StyledTableCell>
+                        <StyledTableCell align="left">{row.vaccine}</StyledTableCell>
+                        <StyledTableCell align="left">{row.batchNumber}</StyledTableCell>
+                        <StyledTableCell align="left">{row.dosage}</StyledTableCell>
+                        <StyledTableCell align="left">{getStatus(row.status)}</StyledTableCell>
                         <StyledTableCell align="left"><Button size="small" onClick={handleOpenVaccine}>View Record</Button></StyledTableCell>
                       </StyledTableRow>
                     ))}
@@ -232,9 +188,6 @@ export default function PassportHolderDashboard() {
             }
         
       </div>
-           
-            
-       
     )
   }
 }
