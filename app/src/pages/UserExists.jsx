03783b4b6834e304fb2eb019c512b61bf34c20f5
RuFerdZ <ge.rusiru@gmail.com>
2022-06-29 01:35:45 +0530

    added user already exists page

---
+++ app/src/pages/UserExists.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+export default function UserExists(){
+    const navigate = useNavigate();
+    return (
+        <div className="text-center">
+            <div className="primary-text main-center-container">User Already Exists</div>
+            <button className="button-secondary py-2"
+                onClick={() => navigate(-1)}
+            >back</button>
+        </div>
+    )
+}
