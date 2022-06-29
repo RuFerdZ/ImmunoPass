ed133bb5f86720ac9ee391505af026f8520838c1
RuFerdZ <ge.rusiru@gmail.com>
2022-06-30 04:50:46 +0530

    updated user not found page

--- app/src/pages/UserNotFound.jsx
+++ app/src/pages/UserNotFound.jsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
 
-export default function UserNotFount(){
-    const navigate = useNavigate();
-    return (
-        <div className="text-center">
-            <div className="primary-text main-center-container">User Not Found</div>
-            <button className="button-secondary py-2"
-                onClick={() => navigate(-1)}
-            >back</button>
-        </div>
-    )
+export default function UserNotFount() {
+  const navigate = useNavigate();
+  return (
+    <div className="text-center">
+      <div className="primary-text main-center-container">User Not Found</div>
+      <button
+        className="button-secondary py-2 mx-auto"
+        onClick={() => navigate(-1)}
+      >
+        back
+      </button>
+    </div>
+  );
 }
